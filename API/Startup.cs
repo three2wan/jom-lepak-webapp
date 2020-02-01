90aa2d31e70f97e6096a06dca2a5bd0f1b6aaca1
Syazwan Jamil <42228200+msyazwan7@users.noreply.github.com>
2020-02-02 06:08:10 +0800

    Create front end using create-react-app

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,15 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -42,7 +51,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
